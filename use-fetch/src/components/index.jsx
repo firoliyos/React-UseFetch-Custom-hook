c781c2b5c7cb439f0dae5fb2d0c6960904da524f
firoliyos <firatesfaye44@gmail.com>
2024-11-03 11:23:55 +0300

    index-done

---
+++ use-fetch/src/components/index.jsx
@@ -0,0 +1,29 @@
+
+import {useState, useEffect} from 'react'
+export default function useFetch({url, options={}}) {
+    
+   const [data, setData] = useState(null)
+   const [pending, setPending] = useState(false)
+   const [error, setError] = useState(null)
+
+   async function fetchData() {
+       setPending(true)
+      try {
+         const response = await fetch(url, {...options})
+         if(!response.ok) throw new Error(response.statusText)
+         const result = await response.json() 
+         setData(result)  
+         setPending(false)
+         setError(null)
+      }catch(error){
+        setError(`${error}. Something went wrong!`)
+        setPending(false)
+      }
+   }
+
+   useEffect(() => {
+       fetchData()
+   }, [url])
+
+   return {data, error, pending}
+}
