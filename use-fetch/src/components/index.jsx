2b6c05b5eb1c5185fca567ed476e10d6f59dcaa7
firoliyos <firatesfaye44@gmail.com>
2024-11-03 11:25:00 +0300

    document-formatted

--- use-fetch/src/components/index.jsx
+++ use-fetch/src/components/index.jsx
@@ -1,29 +1,28 @@
+import { useState, useEffect } from "react";
 
-import {useState, useEffect} from 'react'
-export default function useFetch({url, options={}}) {
-    
-   const [data, setData] = useState(null)
-   const [pending, setPending] = useState(false)
-   const [error, setError] = useState(null)
+export default function useFetch({ url, options = {} }) {
+  const [data, setData] = useState(null);
+  const [pending, setPending] = useState(false);
+  const [error, setError] = useState(null);
 
-   async function fetchData() {
-       setPending(true)
-      try {
-         const response = await fetch(url, {...options})
-         if(!response.ok) throw new Error(response.statusText)
-         const result = await response.json() 
-         setData(result)  
-         setPending(false)
-         setError(null)
-      }catch(error){
-        setError(`${error}. Something went wrong!`)
-        setPending(false)
-      }
-   }
+  async function fetchData() {
+    setPending(true);
+    try {
+      const response = await fetch(url, { ...options });
+      if (!response.ok) throw new Error(response.statusText);
+      const result = await response.json();
+      setData(result);
+      setPending(false);
+      setError(null);
+    } catch (error) {
+      setError(`${error}. Something went wrong!`);
+      setPending(false);
+    }
+  }
 
-   useEffect(() => {
-       fetchData()
-   }, [url])
+  useEffect(() => {
+    fetchData();
+  }, [url]);
 
-   return {data, error, pending}
+  return { data, error, pending };
 }
